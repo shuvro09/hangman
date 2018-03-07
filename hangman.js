a1d692716db9822a0ebac3cf3f9bebaa3a64d911
Shuvrajyoti Paul <33157366+shuvro09@users.noreply.github.com>
2018-03-08 00:15:18 +0530

    Add files via upload

---
+++ hangman.js
@@ -0,0 +1,178 @@
+const start=$("#start");
+const gamePage=$("#gamePage");
+const startPage=$("#startPage");
+const home=$(".home")
+const gameOver=$("#gameOver")
+const canvasPage=$("#canvasPage")
+let strike=0
+let index=0
+let answer=[]
+var words=['STRONG','SWING','FALSE','TREE','SCIENTIST','ABYSS','BAGPIPES','IVY','PNEUMONIA','PUPPY','CAT']
+var arr=[]
+var checkWord
+/*$(document).ready(function(){
+    gamePage.hide();
+    gameOver.hide();
+    canvasPage.hide();
+    
+})*/
+function clearCanvas(){
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.clearRect(0, 0, c.width, c.height);
+    ctx.beginPath();
+    
+}
+function initialDraw(){
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    
+    ctx.moveTo(50,50);
+    ctx.lineTo(50,200);
+    ctx.stroke();
+    ctx.moveTo(50,50);
+    ctx.lineTo(150,50);
+    ctx.stroke();
+    ctx.moveTo(25,200);
+    ctx.lineTo(75,200);
+    ctx.stroke();
+    ctx.moveTo(120,50);
+    ctx.lineTo(120,70);
+    ctx.stroke();
+    
+
+}
+function canvasfill(str){
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    if(str===1)
+    {
+        ctx.moveTo(120,50);
+        ctx.beginPath();
+        ctx.arc(120,85,15,0,2*Math.PI);
+        ctx.stroke();
+    }
+    else if(str===2)
+    {
+        ctx.moveTo(120,100);
+        ctx.lineTo(120,140);
+        ctx.stroke();
+    }
+    else if(str===3)
+    {
+        ctx.moveTo(120,120);
+        ctx.lineTo(100,110);
+        ctx.stroke();
+    }
+    else if(str===4)
+    {
+        ctx.moveTo(120,120);
+        ctx.lineTo(140,110);
+        ctx.stroke();
+    }
+    else if(str===5)
+    {
+        ctx.moveTo(120,140);
+        ctx.lineTo(140,160);
+        ctx.stroke();
+    }
+    else
+    {
+        ctx.moveTo(120,140);
+        ctx.lineTo(100,160);
+        ctx.stroke();
+    }
+
+}
+
+function selectWord(){
+    return (words[index]);
+}
+function showVal(word){
+    
+    for(var i=0;i<word.length;i++)
+        {
+            arr.push("_");
+            arr.push(" ");
+            answer.push(" ")
+        }
+    $("#answer").val((arr.toString()).replace(/\,/g, ""))
+    
+}
+function newgame(){
+    
+    startPage.hide();
+    gamePage.show();
+    canvasPage.show();
+    
+    arr.length=0;
+    answer.length=0;
+    strike=0
+    $("td").css("background-color","white")
+    $("td").data('clicked',false)
+    checkWord=selectWord()
+    showVal(checkWord)
+    clearCanvas()
+    initialDraw()
+}
+start.click(newgame)
+
+
+home.click(function(){
+    index=0;
+    
+    gameOver.hide();
+    canvasPage.hide();
+    gamePage.hide();
+    startPage.show();
+})
+function checker(check){
+    
+    if(checkWord===check)
+        {
+            alert("Congrats!You Won.The word was :"+checkWord);
+            index++;
+            if(index==words.length){
+                alert("You completed the game!!");
+                gamePage.hide();
+                index=0
+                gameOver.show();
+            }
+            else
+            newgame()
+            
+        }
+}
+$("table").find('td').click(function(){
+    
+    let x=$(this).text();
+    
+    let flag=0
+    if(!$(this).data('clicked')){
+        for(var i=0;i<checkWord.length;i++){
+            if(checkWord.charAt(i)==x){
+                
+                arr[i*2]=x
+               
+                answer[i]=x;
+                flag=1
+                $(this).css("background-color",'green')
+            }
+             
+        }
+        
+        if(flag!==1){strike++;$(this).css("background-color",'red');canvasfill(strike)}
+        if(strike===6){
+            gamePage.hide();
+            index=0;
+            gameOver.show();
+            canvasPage.show();
+        }
+        
+        $(this).data('clicked',true);
+        $("#answer").val((arr.toString()).replace(/\,/g, ""))
+        //
+        
+    }
+    checker((answer.toString()).replace(/\,/g, ""))
+})
